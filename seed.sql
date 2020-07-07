dd01ed0fa28a095d7b7df318aa70eb7dfdae018b
lateef10 <40859670+lateef10@users.noreply.github.com>
2020-07-07 11:37:34 -0400

    seed query

---
+++ seed.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS Category(id INTEGER PRIMARY KEY AUTOINCREMENT,CategoryName TEXT);
+INSERT or IGNORE INTO Category VALUES (1, 'Start up');
+INSERT or IGNORE INTO Category VALUES (2, 'Programming');
+ 
+CREATE TABLE IF NOT EXISTS MyWebsite(id INTEGER PRIMARY KEY AUTOINCREMENT, myWebId INTEGER, websiteName TEXT, [url] TEXT);
+INSERT or IGNORE INTO MyWebsite(id, myWebId, websiteName, [url]) VALUES (1, 1, 'Amazon', 'https://amazon.com');
+INSERT or IGNORE INTO MyWebsite(id, myWebId, websiteName, [url]) VALUES (2, 1, 'Reddit', 'https://reddit.com');
+INSERT or IGNORE INTO MyWebsite(id, myWebId, websiteName, [url]) VALUES (3, 2, 'Stackoverflow', 'https://stackoverflow.com');
+INSERT or IGNORE INTO MyWebsite(id, myWebId, websiteName, [url]) VALUES (4, 2, 'Educative io', 'https://educative.io');
+INSERT or IGNORE INTO MyWebsite(id, myWebId, websiteName, [url]) VALUES (5, 2, 'Cloudinary', 'https://cloudinary.com');
